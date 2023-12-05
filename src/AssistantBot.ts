b5881b2d005d5e4a3bfc9283bd92b5b057161ee1
Ken_Cir <65713982+KenCir@users.noreply.github.com>
2023-12-05 22:40:39 +0900

    fix: Logger name

--- src/AssistantBot.ts
+++ src/AssistantBot.ts
@@ -3,91 +3,102 @@ import { getLogger, configure, shutdown, Logger } from 'log4js';
 import { Command } from './interfaces/Command';
 
 export default class AssistantBot extends Client {
-  /**
-   * ロガー
-   */
-  public readonly logger!: Logger;
+    /**
+     * ロガー
+     */
+    public readonly logger!: Logger;
 
-  /**
-   * コマンドのコレクション
-   */
-  public readonly commands!: Collection<string, Command>;
+    /**
+     * コマンドのコレクション
+     */
+    public readonly commands!: Collection<string, Command>;
 
-  /**
-   * テストチャンネル(テキスト)を作成するチャンネルのID
-   */
-  public readonly createTextTestChannel!: string;
+    /**
+     * テストチャンネル(テキスト)を作成するチャンネルのID
+     */
+    public readonly createTextTestChannel!: string;
 
-  /**
-   * テストチャンネル(テキスト)を作成するカテゴリのID
-   */
-  public readonly testTextChannelCategory!: string;
+    /**
+     * テストチャンネル(テキスト)を作成するカテゴリのID
+     */
+    public readonly testTextChannelCategory!: string;
 
-  /**
-   * テストチャンネル(ボイス)を作成するチャンネルのID
-   */
-  public readonly createVoiceTestChannel!: string;
+    /**
+     * テストチャンネル(ボイス)を作成するチャンネルのID
+     */
+    public readonly createVoiceTestChannel!: string;
 
-  /**
-   * テストチャンネル(ボイス)を作成するカテゴリのID
-   */
-  public readonly testVoiceChannelCategory!: string;
+    /**
+     * テストチャンネル(ボイス)を作成するカテゴリのID
+     */
+    public readonly testVoiceChannelCategory!: string;
 
-  /**
-   * テストチャンネル(ステージ)を作成するチャンネルのID
-   */
-  public readonly createStageTestChannel!: string;
+    /**
+     * テストチャンネル(ステージ)を作成するチャンネルのID
+     */
+    public readonly createStageTestChannel!: string;
 
-  /**
-   * テストチャンネル(ステージ)を作成するカテゴリのID
-   */
-  public readonly testStageChannelCategory!: string;
+    /**
+     * テストチャンネル(ステージ)を作成するカテゴリのID
+     */
+    public readonly testStageChannelCategory!: string;
 
-  /**
-   * 無効なテストチャンネルのID、消しちゃいけないチャンネルとか
-   */
-  public readonly disableTestChannels!: Array<string>;
+    /**
+     * 無効なテストチャンネルのID、消しちゃいけないチャンネルとか
+     */
+    public readonly disableTestChannels!: Array<string>;
 
-  public constructor() {
-    super({
-      intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildIntegrations,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-      ],
-      allowedMentions: {
-        parse: ['users'],
-        repliedUser: false,
-      },
-    });
+    public constructor() {
+        super({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildIntegrations,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.GuildMembers,
+            ],
+            allowedMentions: {
+                parse: ['users'],
+                repliedUser: false,
+            },
+        });
 
-    configure({
-      appenders: {
-        out: { type: 'stdout', layout: { type: 'coloured' } },
-        app: { type: 'file', filename: 'logs/eewbot.log', pattern: 'yyyy-MM-dd.log' },
-      },
-      categories: {
-        default: { appenders: ['out', 'app'], level: 'all' },
-      },
-    });
+        configure({
+            appenders: {
+                out: { type: 'stdout', layout: { type: 'coloured' } },
+                app: {
+                    type: 'file',
+                    filename: 'logs/discord-assistant-bot.log',
+                    pattern: 'yyyy-MM-dd.log',
+                },
+            },
+            categories: {
+                default: { appenders: ['out', 'app'], level: 'all' },
+            },
+        });
 
-    this.logger = getLogger('EEWBot');
-    this.commands = new Collection();
+        this.logger = getLogger('DiscordAssistantBot');
+        this.commands = new Collection();
 
-    this.createTextTestChannel = '1105882049535021127';
-    this.testTextChannelCategory = '1105881905586516028';
-    this.createVoiceTestChannel = '1107289762999574651';
-    this.testVoiceChannelCategory = '1107276410453758013';
-    this.createStageTestChannel = '1107290871549923328';
-    this.testStageChannelCategory = '1107276410453758013';
-    this.disableTestChannels = ['972836497747226654', '1105882049535021127', '1107289762999574651', '1107290871549923328', '1107289865235734568', '1107289828623667220'];
-  }
+        this.createTextTestChannel = '1105882049535021127';
+        this.testTextChannelCategory = '1105881905586516028';
+        this.createVoiceTestChannel = '1107289762999574651';
+        this.testVoiceChannelCategory = '1107276410453758013';
+        this.createStageTestChannel = '1107290871549923328';
+        this.testStageChannelCategory = '1107276410453758013';
+        this.disableTestChannels = [
+            '972836497747226654',
+            '1105882049535021127',
+            '1107289762999574651',
+            '1107290871549923328',
+            '1107289865235734568',
+            '1107289828623667220',
+        ];
+    }
 
-  public shutdown(): void {
-    this.logger.info('シャットダウンしています...');
-    this.destroy();
-    shutdown();
-  }
+    public shutdown(): void {
+        this.logger.info('シャットダウンしています...');
+        this.destroy();
+        shutdown();
+    }
 }
