90a2a18da9651a2a363b0185364c2677f7903523
Ken_Cir <65713982+KenCir@users.noreply.github.com>
2023-12-09 19:47:29 +0900

    fix: ApplicationCommandData Types

--- src/utils/registerSlashCommand.ts
+++ src/utils/registerSlashCommand.ts
@@ -1,38 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment */
 import { config } from 'dotenv';
-import { REST, Routes } from 'discord.js';
+import { ApplicationCommandDataResolvable, REST, Routes } from 'discord.js';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 import { Command } from '../interfaces/Command';
-import { SlashCommandBuilder } from 'discord.js';
 config();
 
 void (async () => {
-  const commands: Array<SlashCommandBuilder> = [];
-  const commandFolders = readdirSync(join(__dirname, '../commands'));
-  for (const folder of commandFolders) {
-    readdirSync(join(__dirname, '../commands/', folder))
-      .forEach(file => {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const command = require(join(__dirname, '../commands/', folder, file));
-        const cmd: Command = new command.default();
-        commands.push(cmd.commandData);
-        console.info(`${cmd.name} command is Loading`);
-      });
-  }
+    const commands: Array<ApplicationCommandDataResolvable> = [];
+    const commandFolders = readdirSync(join(__dirname, '../commands'));
+    for (const folder of commandFolders) {
+        readdirSync(join(__dirname, '../commands/', folder)).forEach((file) => {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            const command = require(join(__dirname, '../commands/', folder, file));
+            const cmd: Command = new command.default();
+            commands.push(cmd.commandData);
+            console.info(`${cmd.name} command is Loading`);
+        });
+    }
 
-  const rest: REST = new REST().setToken(process.env.DISCORD_TOKEN as string);
-  try {
-    console.log('Started refreshing application (/) commands.');
+    const rest: REST = new REST().setToken(process.env.DISCORD_TOKEN as string);
+    try {
+        console.log('Started refreshing application (/) commands.');
 
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID as string),
-      { body: commands },
-    );
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID as string), { body: commands });
 
-    console.log('Successfully reloaded application (/) commands.');
-  }
-  catch (error) {
-    console.error(error);
-  }
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
 })();
