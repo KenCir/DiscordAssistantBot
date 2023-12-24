40002c5aedfac0b34359fc0051e84b60d841d040
Ken_Cir <65713982+KenCir@users.noreply.github.com>
2023-12-25 02:45:45 +0900

    fix: types

--- src/events/discord/interactionCreate.ts
+++ src/events/discord/interactionCreate.ts
@@ -2,11 +2,11 @@ import { Interaction, InteractionType } from 'discord.js';
 import AssistantBot from '../../AssistantBot';
 
 export default async (client: AssistantBot, interaction: Interaction) => {
-  if (interaction.user.bot || !interaction.guild || interaction.type !== InteractionType.ApplicationCommand) return;
+    if (!interaction.isChatInputCommand()) return;
 
-  const cmd = client.commands.get(interaction.commandName);
-  if (!cmd) return await interaction.reply('ERROR: コマンドデータが見つかりませんでした');
-  if (cmd.deferReply) await interaction.deferReply();
+    const cmd = client.commands.get(interaction.commandName);
+    if (!cmd) return await interaction.reply('ERROR: コマンドデータが見つかりませんでした');
+    if (cmd.deferReply) await interaction.deferReply();
 
-  await cmd.execute(client, interaction);
+    await cmd.execute(client, interaction);
 };
